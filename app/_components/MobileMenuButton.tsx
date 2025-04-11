5bfe2f00cd463a2420edcadfee5f19a57f88495d
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-11 12:52:21 -0300

    Created Button component to open mobile menu

---
+++ app/_components/MobileMenuButton.tsx
@@ -0,0 +1,24 @@
+import { Bars4Icon, XMarkIcon } from "@heroicons/react/20/solid";
+
+type MobileMenuButtonProps = {
+  menuIsOpen: boolean;
+  setMenuIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export function MobileMenuButton({
+  menuIsOpen,
+  setMenuIsOpen,
+}: MobileMenuButtonProps) {
+  return (
+    <button
+      className="ml-auto cursor-pointer p-2 text-gray-700 sm:hidden"
+      onClick={() => setMenuIsOpen((prev) => !prev)}
+    >
+      {menuIsOpen ? (
+        <XMarkIcon className="h-6 w-6 text-stone-600" />
+      ) : (
+        <Bars4Icon className="h-6 w-6 text-stone-600" />
+      )}
+    </button>
+  );
+}
