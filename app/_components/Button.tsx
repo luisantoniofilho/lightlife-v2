60b162862e79413e491e51505f6d45d3d0328e1a
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-15 14:20:55 -0300

    Reusable Button component updated

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,8 +1,18 @@
 import { ReactNode } from "react";
 
-export default function Button({ children }: { children: ReactNode }) {
+type ButtonProps = {
+  children: ReactNode;
+  type: "button" | "submit" | "reset";
+  onClick?: () => void;
+};
+
+export default function Button({ children, type, onClick }: ButtonProps) {
   return (
-    <button className="rounded-full bg-green-600 px-8 py-3 text-lg tracking-widest text-white transition-all hover:bg-green-700">
+    <button
+      className="cursor-pointer rounded-full bg-green-600 px-8 py-3 text-lg tracking-widest text-white transition-all hover:bg-green-700 sm:text-xl md:text-2xl"
+      type={type}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
