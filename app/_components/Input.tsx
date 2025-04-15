6a2489f266d55f6be4f6b0219656c9861838e137
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-15 14:26:15 -0300

    Changed text increase to sm breakpoint

--- app/_components/Input.tsx
+++ app/_components/Input.tsx
@@ -8,14 +8,14 @@ type inputProps = {
 export default function Input({ label, type, name, placeholder }: inputProps) {
   return (
     <div>
-      <label className="block font-medium text-gray-700 md:text-xl">
+      <label className="block font-medium text-gray-700 sm:text-xl">
         {label}
       </label>
       <input
         type={type}
         name={name}
         placeholder={placeholder}
-        className="mt-1 w-full rounded-lg border border-gray-300 p-2 focus:ring focus:ring-green-300 md:text-xl"
+        className="mt-1 w-full rounded-lg border border-gray-300 p-2 focus:ring focus:ring-green-300 sm:text-xl"
       />
     </div>
   );
