3c4cb5bd5154323a0cbeb9a5a3b98ea9f3fe5629
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-14 22:53:07 -0300

    Errors checks added to spoonacular.ts

--- app/_lib/spoonacular.ts
+++ app/_lib/spoonacular.ts
@@ -1,7 +1,16 @@
 export async function getMeals(calories: number) {
-  const meals = await fetch(
-    `https://api.spoonacular.com/mealplanner/generate?targetCalories=${calories}&timeFrame=day&apiKey=${process.env.SPOONACULAR_API_KEY}`,
-  );
+  const apiKey = process.env.SPOONACULAR_API_KEY;
+  const url = `https://api.spoonacular.com/mealplanner/generate?targetCalories=${calories}&timeFrame=day&apiKey=${apiKey}`;
 
-  return await meals.json();
+  const res = await fetch(url);
+
+  // Check if response is ok
+  if (!res.ok) {
+    const errorText = await res.text();
+    console.error("Error calling Spoonacular:", res.status, res.statusText);
+    console.error("Response:", errorText);
+    throw new Error(`API ERROR: ${res.status} ${res.statusText}`);
+  }
+
+  return await res.json();
 }
