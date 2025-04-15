c178e35a349f06e8ea78bef90a7c841328fafe7e
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-15 19:53:13 -0300

    SpinnerMini color updated

--- app/_components/SpinnerMini.tsx
+++ app/_components/SpinnerMini.tsx
@@ -1,5 +1,5 @@
 export default function SpinnerMini() {
   return (
-    <div className="border-primary-200 m-0 aspect-square w-5 animate-spin rounded-full border-2 border-r-transparent"></div>
+    <div className="border-primary-900 m-0 aspect-square w-5 animate-spin rounded-full border-2 border-r-transparent"></div>
   );
 }
