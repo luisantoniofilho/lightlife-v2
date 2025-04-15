b68f47d96dd4f65edf2d3963c8fa53ea69ed628e
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-15 19:51:11 -0300

    SpinnerMini component created

---
+++ app/_components/SpinnerMini.tsx
@@ -0,0 +1,5 @@
+export default function SpinnerMini() {
+  return (
+    <div className="border-primary-200 m-0 aspect-square w-5 animate-spin rounded-full border-2 border-r-transparent"></div>
+  );
+}
