8697297300c5e6d9562d900ca4ebf74b337ca81d
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-15 12:24:17 -0300

    Text size increased on lg berakpoint

--- app/_components/HeaderLink.tsx
+++ app/_components/HeaderLink.tsx
@@ -12,7 +12,7 @@ export function HeaderLink({ href, children, onClick }: HeaderLinkProps) {
     <Link
       href={href}
       onClick={onClick}
-      className="hover:bg-primary-900 rounded-full px-4 py-2 text-base text-stone-600 transition-all duration-300 hover:text-white"
+      className="hover:bg-primary-900 rounded-full px-4 py-2 text-base text-stone-600 transition-all duration-300 hover:text-white lg:text-lg"
     >
       {children}
     </Link>
