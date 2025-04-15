3c5e68cb45febf0431440e37408b53cf07aeedfc
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-15 12:53:31 -0300

    Text size increased on md breakpoint

--- app/_components/Input.tsx
+++ app/_components/Input.tsx
@@ -8,12 +8,14 @@ type inputProps = {
 export default function Input({ label, type, name, placeholder }: inputProps) {
   return (
     <div>
-      <label className="block font-medium text-gray-700">{label}</label>
+      <label className="block font-medium text-gray-700 md:text-xl">
+        {label}
+      </label>
       <input
         type={type}
         name={name}
         placeholder={placeholder}
-        className="mt-1 w-full rounded-lg border border-gray-300 p-2 focus:ring focus:ring-green-300"
+        className="mt-1 w-full rounded-lg border border-gray-300 p-2 focus:ring focus:ring-green-300 md:text-xl"
       />
     </div>
   );
