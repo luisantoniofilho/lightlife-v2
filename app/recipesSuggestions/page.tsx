135f0c4d14051a5640c0fcf903bf37c9687dc978
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-16 00:25:35 -0300

    feat: added a caption component to recipeSuggestions page

---
+++ app/recipesSuggestions/page.tsx
@@ -0,0 +1,83 @@
+import { auth } from "@/_lib/auth";
+import { getUserData } from "@/_lib/firebaseActions";
+import { getMeals } from "@/_lib/spoonacular";
+import { ReactNode } from "react";
+import MealItem from "./Recipe";
+
+type MealData = {
+  id: number;
+  image: string;
+  imageType: string;
+  readyInMinutes: number;
+  servings: number;
+  sourceUrl: string;
+  title: string;
+};
+
+async function Page() {
+  const session = await auth();
+
+  if (!session?.user?.email) throw new Error("User not authenticated");
+
+  const userData = await getUserData(session?.user?.email);
+
+  const totalCalories = userData!.totalCalories;
+
+  const data = await getMeals(totalCalories);
+  const { meals, nutrients } = data;
+
+  function Caption({
+    label,
+    children,
+  }: {
+    label: string;
+    children: ReactNode;
+  }) {
+    return (
+      <div className="rounded-lg p-4">
+        <h3 className="font-semibold md:text-xl">{label}</h3>
+        <p className="md:text-lg">{children}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="mb-6">
+        <h2 className="mb-2 text-2xl font-bold md:text-3xl">
+          Recipes Suggestion total nutrients
+        </h2>
+        <div className="grid grid-cols-2 gap-4">
+          <Caption label="Calories">
+            {nutrients.calories.toFixed(2)} kcal
+          </Caption>
+          <Caption label="Carbohydrates">
+            {nutrients.carbohydrates.toFixed(2)} g
+          </Caption>
+          <Caption label="Fat">{nutrients.fat.toFixed(2)} g</Caption>
+          <Caption label="Protein">{nutrients.protein.toFixed(2)} g</Caption>
+        </div>
+      </div>
+
+      <div className="mb-8">
+        <h1 className="mb-4 text-2xl font-bold md:text-3xl">
+          Recipe suggestions
+        </h1>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+          {meals.map((meal: MealData) => (
+            <MealItem
+              key={meal.id}
+              title={meal.title}
+              image={meal.image}
+              readyInTime={meal.readyInMinutes}
+              servings={meal.servings}
+              sourceUrl={meal.sourceUrl}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
