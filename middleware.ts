3e089473e436a38ad7ff8e9d281f0706e8863f8a
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-16 14:01:19 -0300

    fix: fix name of recipeSuggestions page on middleware

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,5 @@ import { auth } from "@/_lib/auth";
 export const middleware = auth;
 
 export const config = {
-  matcher: ["/form", "/macronutrients", "/mealsSuggestions"],
+  matcher: ["/form", "/macronutrients", "/recipeSuggestions"],
 };
