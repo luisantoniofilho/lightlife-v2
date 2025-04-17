69333542ed12e4cbcf68d55b7661a3753f06321c
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-16 21:45:27 -0300

    refactor: move form to it own component UserForm and import it on form page

---
+++ app/form/UserForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Button from "@/_components/Button";
+import Input from "@/_components/Input";
+import Select from "@/_components/Select";
+import { calcMacrosAction } from "@/_lib/actions";
+import { useTransition } from "react";
+
+export default function UserForm() {
+  const [isPending, startTransition] = useTransition();
+
+  function handleSubmit(formData: FormData) {
+    startTransition(() => {
+      calcMacrosAction(formData);
+    });
+  }
+
+  return (
+    <form
+      className="grid grid-cols-1 justify-center gap-4 rounded-lg bg-white p-6 shadow-md sm:grid-cols-2 sm:gap-10 md:px-16"
+      action={handleSubmit}
+    >
+      {/* Age */}
+      <Input
+        label="Age"
+        type="number"
+        name="age"
+        placeholder="Enter your age"
+      />
+
+      {/* Weight */}
+      <Input
+        label="Weight (kg)"
+        type="number"
+        name="weight"
+        placeholder="Ex: 70"
+      />
+
+      {/* Height */}
+      <Input
+        label="Height (cm)"
+        type="number"
+        name="height"
+        placeholder="Ex: 175"
+      />
+
+      {/* Gender */}
+      <Select label="Gender" name="gender" options={["male", "female"]} />
+
+      {/* Activity level */}
+      <Select
+        label="Activity level"
+        name="activityLevel"
+        options={["sedentary", "light", "moderate", "active", "very-active"]}
+      />
+
+      {/* Goal */}
+      <Select
+        label="Goal"
+        name="goal"
+        options={["gain-muscle", "lose-fat", "mantain"]}
+      />
+
+      {/* Send button */}
+      <div className="flex justify-center sm:col-span-2">
+        <Button type="submit" disable={isPending}>
+          {isPending ? "Calculating macros..." : "Send form"}
+        </Button>
+      </div>
+    </form>
+  );
+}
--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,7 +1,4 @@
-import Button from "@/_components/Button";
-import Input from "@/_components/Input";
-import Select from "@/_components/Select";
-import { calcMacrosAction } from "@/_lib/actions";
+import UserForm from "./UserForm";
 
 export default function Page() {
   return (
@@ -10,56 +7,7 @@ export default function Page() {
         Enter your physical data
       </h2>
 
-      <form
-        className="grid grid-cols-1 justify-center gap-4 rounded-lg bg-white p-6 shadow-md sm:grid-cols-2 sm:gap-10 md:px-16"
-        action={calcMacrosAction}
-      >
-        {/* Age */}
-        <Input
-          label="Age"
-          type="number"
-          name="age"
-          placeholder="Enter your age"
-        />
-
-        {/* Weight */}
-        <Input
-          label="Weight (kg)"
-          type="number"
-          name="weight"
-          placeholder="Ex: 70"
-        />
-
-        {/* Height */}
-        <Input
-          label="Height (cm)"
-          type="number"
-          name="height"
-          placeholder="Ex: 175"
-        />
-
-        {/* Gender */}
-        <Select label="Gender" name="gender" options={["male", "female"]} />
-
-        {/* Activity level */}
-        <Select
-          label="Activity level"
-          name="activityLevel"
-          options={["sedentary", "light", "moderate", "active", "very-active"]}
-        />
-
-        {/* Goal */}
-        <Select
-          label="Goal"
-          name="goal"
-          options={["gain-muscle", "lose-fat", "mantain"]}
-        />
-
-        {/* Send button */}
-        <div className="flex justify-center sm:col-span-2">
-          <Button type="submit">Send form</Button>
-        </div>
-      </form>
+      <UserForm />
     </div>
   );
 }
