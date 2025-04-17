cab9b6fc4629cf5f961d1266b46b784b5d899932
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-16 21:44:02 -0300

    refactor: add a disabled optional prop on the Button component

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -3,14 +3,21 @@ import { ReactNode } from "react";
 type ButtonProps = {
   children: ReactNode;
   type: "button" | "submit" | "reset";
+  disable?: boolean;
   onClick?: () => void;
 };
 
-export default function Button({ children, type, onClick }: ButtonProps) {
+export default function Button({
+  children,
+  type,
+  disable,
+  onClick,
+}: ButtonProps) {
   return (
     <button
       className="cursor-pointer rounded-full bg-green-600 px-8 py-3 text-xl tracking-widest text-white transition-all hover:bg-green-700 md:text-2xl"
       type={type}
+      disabled={disable ? disable : false}
       onClick={onClick}
     >
       {children}
