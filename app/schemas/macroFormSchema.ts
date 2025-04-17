84015a64b6fc295b5272adaabdcddb37ef589b8c
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-17 12:45:20 -0300

    feat: create a schema for the MacroFormData

---
+++ app/schemas/macroFormSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const macroFormSchema = z.object({
+  age: z.coerce.number().min(10).max(100),
+  weight: z.coerce.number().min(30).max(300),
+  height: z.coerce.number().min(100).max(250),
+  gender: z.enum(["male", "female"]),
+  activityLevel: z.enum(["sedentary", "light", "moderate", "active"]),
+  goal: z.enum(["gain-muscle", "lose-fat", "maintain"]),
+});
+
+export type MacroFormData = z.infer<typeof macroFormSchema>;
