7fc3338e1923bc45eb2a8619ad4d03049fc20044
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-17 21:22:44 -0300

    style: remove footer border

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="border-t border-stone-50 py-4 text-center">
+    <footer className="py-4 text-center">
       <span className="text-primary-900">
         © {new Date().getFullYear()} LightLife - All rights reserved.
       </span>
