49de0c082db1b759532e23cfeb1df2db741c7535
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-17 21:29:42 -0300

    fix: remove redundant text-size on form page h2

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -3,7 +3,7 @@ import UserForm from "./UserForm";
 export default function Page() {
   return (
     <div>
-      <h2 className="mb-8 text-center text-3xl font-bold sm:mb-16 md:text-3xl">
+      <h2 className="mb-8 text-center text-3xl font-bold sm:mb-16">
         Enter your physical data
       </h2>
 
