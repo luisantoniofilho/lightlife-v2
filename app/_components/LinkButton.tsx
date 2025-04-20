a5e96d5c44942bcfd5295c99f7b4fc3f791c2ad0
Luis Antonio <luissantosfilho2014@gmail.com>
2025-04-19 23:10:28 -0300

    feat: add mx -auto, text-center and a prop called optionalClassName to the LinkButton

--- app/_components/LinkButton.tsx
+++ app/_components/LinkButton.tsx
@@ -4,13 +4,18 @@ import { ReactNode } from "react";
 type buttonProps = {
   href: string;
   children: ReactNode;
+  optionalClassName?: string;
 };
 
-export default function LinkButton({ href, children }: buttonProps) {
+export default function LinkButton({
+  href,
+  children,
+  optionalClassName,
+}: buttonProps) {
   return (
     <Link
       href={href}
-      className="flex cursor-pointer gap-2 rounded-full bg-green-600 px-8 py-3 text-lg tracking-widest text-white transition-all hover:bg-green-700 lg:text-2xl"
+      className={`mx-auto flex cursor-pointer gap-2 rounded-full bg-green-600 px-8 py-3 text-center text-lg tracking-widest text-white transition-all hover:bg-green-700 lg:text-2xl ${optionalClassName}`}
     >
       {children}
     </Link>
