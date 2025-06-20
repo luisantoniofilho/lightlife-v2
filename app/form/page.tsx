e028240048b78de79e99374fbcc0dea3b809da97
Luis Antonio <luissantosfilho2014@gmail.com>
2025-06-20 14:18:24 -0300

    feat: add auth check on form page

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@/_lib/auth";
 import UserForm from "./UserForm";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  // Authentication check
+  const session = await auth();
+  if (!session?.user?.email) redirect("/login");
+
   return (
     <div>
       <h2 className="mb-8 text-center text-3xl font-bold sm:mb-16">
