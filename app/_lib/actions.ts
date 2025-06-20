37d16eebd5cf2231f7eb8834b5bf41c248751d9e
Luis Antonio <luissantosfilho2014@gmail.com>
2025-06-20 14:10:23 -0300

    refactor: update actions.ts to use mongodbActions

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -5,8 +5,13 @@ import { calcMetabolicBasalRate } from "@/utils/calcMetabolicBasalRate";
 import { calcTotalCalories } from "@/utils/calcTotalCalories";
 import { redirect } from "next/navigation";
 import { auth, signIn, signOut } from "./auth";
-import { saveUserData } from "./firebaseActions";
+import { saveUserData } from "./mongodb/mongodbActions";
 import { macroFormSchema } from "@/schemas/macroFormSchema";
+import { z } from "zod/v4";
+
+/* //////////////////
+// AUTH ACTIONS
+*/ //////////////////
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/form" });
@@ -16,15 +21,23 @@ export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
 
+/* //////////////////
+// MACROS ACTIONS
+*/ //////////////////
+
 export async function calcMacrosAction(formData: FormData) {
+  // Checking if the user is authenticated
+  const session = await auth();
+  const user = session?.user;
+  if (!user?.email) throw new Error("User not authenticated");
+
   // Transform FormData in object
   const rawData = Object.fromEntries(formData.entries());
 
   const parsed = macroFormSchema.safeParse(rawData);
-
   if (!parsed.success) {
-    console.error(parsed.error.flatten());
-    return { error: parsed.error.flatten() };
+    console.error(z.treeifyError(parsed.error));
+    return { error: z.prettifyError(parsed.error) };
   }
 
   // Taking the form data
@@ -44,13 +57,8 @@ export async function calcMacrosAction(formData: FormData) {
   // Calculating total calories
   const totalCalories = calcTotalCalories({ carbs, protein, fat });
 
-  // Checking if the user is authenticated
-  const session = await auth();
-  const user = session?.user;
-  if (!user?.email) throw new Error("User not authenticated");
-
   // Updating user's macros and calories (was null)
-  await saveUserData(user.email, {
+  await saveUserData({
     goal,
     macros: { carbs, protein, fat },
     totalCalories,
